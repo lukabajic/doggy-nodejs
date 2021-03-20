c58bb0b1cf22032f81dd42f39440602d38df5ab3
lukabajic <lukabajic.dev@gmail.com>
2021-03-20 14:53:06 +0100

    Ss

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("Hello world");
+const express = require("express");
+
+const app = express();
+
+app.use("/", (req, res, next) => {
+  res.json({
+    message: "Bravo",
+  });
+});
+
+app.listen(process.env.PORT || 8000, () => {
+  console.log("Server started");
+});
