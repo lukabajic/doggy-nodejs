16fbadcb085b1bd6949ebdf2bf825374dc0ae633
lukabajic <lukabajic.dev@gmail.com>
2021-03-20 16:09:37 +0100

    Render Index

--- index.js
+++ index.js
@@ -1,13 +1,41 @@
 const express = require("express");
 
+require("dotenv").config();
+
+const port = process.env.port || 8080;
+const serverUrl =
+  process.env.CONFIG === "prod"
+    ? `http://doggyapp-api.epicss.dev:${port}`
+    : `http://localhost:${port}`;
+
 const app = express();
 
-app.use("/", (req, res, next) => {
-  res.json({
-    message: "Bravo",
-  });
+// parses all incoming json data
+app.use(express.json());
+
+// use ejs as view engine
+app.set("view engine", "ejs");
+
+// index page
+app.get("/", (req, res) => {
+  res.render("index");
 });
 
-app.listen(process.env.PORT || 8080, () => {
-  console.log("Server started", process.env.PORT || 8080);
+// CORS
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Methods", "*");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
+app.listen(port, () => {
+  console.log(
+    `Listening to requests on: ${serverUrl} with '${process.env.CONFIG}' config.`
+  );
 });
